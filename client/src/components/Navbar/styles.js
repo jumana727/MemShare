0d0736b2bb3ca9ca32e3109463e65916b7722be1
jumana727 <jumanajambu23@gmail.com>
2022-06-21 20:25:55 +0530

    commit

---
+++ client/src/components/Navbar/styles.js
@@ -0,0 +1,65 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { deepPurple } from '@material-ui/core/colors';
+
+export default makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 5,
+    margin: '10px 0 30px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px 50px',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+    },
+  },
+  heading: {
+    color: 'rgba(0,183,255, 1)',
+    textDecoration: 'none',
+  },
+
+  image: {
+    marginLeft: '0px',
+    marginTop: '0px',
+    textDecoration: 'none',
+    height: '100px',
+    width: '100px',
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    width: '400px',
+    [theme.breakpoints.down('sm')]: {
+      width: 'auto',
+    },
+  },
+  profile: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '400px',
+    alignItems: 'center',
+    [theme.breakpoints.down('sm')]: {
+      width: 'auto',
+      marginTop: 20,
+      justifyContent: 'center',
+    },
+  },
+  logout: {
+    marginLeft: '20px',
+  },
+  userName: {
+    display: 'flex',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  brandContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none',
+  },
+  purple: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+  },
+}));
